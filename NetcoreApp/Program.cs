6d15b55223745242bcee67b5931a33ec1f1ba221
LiemTech <liem.nguyen@student.vives.be>
2022-01-20 13:27:05 +0100

    2C

---
+++ NetcoreApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestArgsInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                System.Console.WriteLine("Please enter parameter values.");
+                Console.Read();
+            }
+            else
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    Console.Write(args[i] + Environment.NewLine);
+                }
+                Console.Read();
+            }
+        }
+    }
+}
